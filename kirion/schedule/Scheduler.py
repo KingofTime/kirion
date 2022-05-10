c410cca9e90c369a995b9ac782906a4fc447622c
KingofTime <antonio_neto77@hotmail.com.br>
2022-05-10 10:23:37 -0300

    Create the specific_horary method
    Create a scheduling functionality at a specific time

--- kirion/schedule/Scheduler.py
+++ kirion/schedule/Scheduler.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import sched
 import time
 from typing import Callable
@@ -8,8 +9,12 @@ class Scheduler(object):
     PRIORITY_HIGH = 0
     PRIORITY_MEDIUM = 1
     PRIORITY_LOW = 2
+    DATE_FORMAT = '%d/%m/%Y'
+    TIME_FORMAT = '%H:%M'
 
     def __init__(self):
+        self.__date = None
+        self.__time = None
         self.__scheduler = sched.scheduler(time.time)
 
     def now(self, action: Callable, priority:int=PRIORITY_HIGH, argument=(), kwargs={}):
@@ -24,5 +29,38 @@ class Scheduler(object):
         self.__scheduler.enter(15, priority, action,
                                argument=argument, kwargs={})
 
+    def specific_horary(self, action, priority=PRIORITY_HIGH, argument=(), kwargs={}):
+        """Method that executes the action in specific horary using the attributes date/time
+
+        Args:
+            action (Callable): task to be performed 
+            priority (int, optional): Priority level of execution. Defaults to PRIORITY_HIGH.
+            argument (tuple, optional): arguments from action callable. Defaults to ().
+            kwargs (dict, optional): key arguments from action callable. Defaults to {}.
+        """
+
+        horary = datetime.combine(self.__date, self.__time)
+        self.__scheduler.enterabs(horary.timestamp(), priority, action, argument=argument, kwargs=kwargs)
+
+
     def run(self):
         self.__scheduler.run(blocking=True)
+    
+    @property
+    def date(self):
+        return self.__date
+
+    @date.setter
+    def date(self, value):
+        self.__date = datetime.strptime(value, self.DATE_FORMAT)
+
+    @property
+    def time(self):
+        return self.__time
+
+    @time.setter
+    def time(self, value):
+        self.__time = datetime.strptime(value, self.TIME_FORMAT).time()
+
+
+
--- main.py
+++ main.py
@@ -8,7 +8,10 @@ def task():
     print("Tarefa bonita")
 
 
-scheduler.now(task)
+scheduler.date = '10/05/2022'
+scheduler.time = '10:21'
+
+scheduler.specific_date(task)
 
 
 scheduler.run()
